748e455ddf23e0a693d8b6913884a128728be81f
AkashChand6n <akashanoop123456@gmail.com>
2025-06-23 04:29:34 +0000

    added

--- community-energy-connect-Final/backend/config/database.php
+++ community-energy-connect-Final/backend/config/database.php
@@ -1,15 +1,19 @@
 <?php
 class Database {
-    private $host = 'localhost';
+    private $host = 'eks-tree-rds.ch2c82wwifaa.us-east-1.rds.amazonaws.com';
     private $db_name = 'community_energy_connect';
-    private $username = 'root';
-    private $password = '';
+    private $username = 'admin';
+    private $password = 'khaleeda';
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
